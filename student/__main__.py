93a9d057de5367820a45c4282f8a92717b398a54
jd@arch <jordangambert@gmail.com>
2026-07-10 12:27:16 +0200

    Parsing args ok

--- student/__main__.py
+++ student/__main__.py
@@ -1,5 +1,5 @@
-# import argparse
-# import asyncio
+from argparse import ArgumentParser
+import asyncio
 
 
 # def REPL():
@@ -14,8 +14,34 @@
 #             break
 
 
+def do_args():
+    parser = ArgumentParser(
+        prog="uv run",
+        description="A project by Nisalmon and Jogamber",
+        epilog="Qu'est-ce que le réel ? Quel est ta définition du réel ?!"
+    )
+    parser.add_argument(
+        "--mcp-stdio",
+        help="Launch the sandbox with the stdio server",
+        required=False
+    )
+    parser.add_argument(
+        "--mcp-server",
+        help="Launch the sandbox with the http server",
+        required=False
+    )
+    parser.add_argument(
+        "config_file",
+        nargs="?",  # option for say 0 or 1 arg
+        help="Path to sandbox config JSON file",
+    )
+    return parser
+
+
 def main():
-    pass
+    parser = do_args()
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == "__main__":
